ce30e300c5d8d472b94ba4c340e6f7ac62aa3399
Jordan Toledo <jordan-direito12@hotmail.com>
2022-10-07 01:30:05 -0300

    Finalizado botoes sidebar

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,4 +1,5 @@
 import os
+from tkinter import VERTICAL
 import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output, State
@@ -17,8 +18,34 @@ import pandas as pd
 
 # ========= Layout ========= #
 layout = dbc.Col([
-                
-            ])
+                html.H1("Minhas Finanças", className="text-primary"),
+                html.P("By Jordan Toledo", className="text-info"),
+                html.Hr(),
+
+# criando botoes 
+                dbc.Button(id='botao_avatar',
+                    children=[html.Img(src='/assets/img_hom.png', alt='Avatar', className='perfil_avatar')
+                ], style={'background-color': 'transparent', 'border-color': 'transparent'}),
+
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Button(color='success', id='open-novo-receita',
+                            children=['Receita'])
+                    ], width=6),
+                    dbc.Col([
+                        dbc.Button(color='danger', id='open-novo-despesa',
+                            children=['Despesa'])
+                    ], width=6)
+                ]),
+# criando navegaçao do menus
+                html.Hr(),
+                dbc.Nav(
+                [
+                    dbc.NavLink("Dashboard", href="/dashboards", active="exact"),
+                    dbc.NavLink("Extratos", href="/extratos", active="exact"),
+                ],  vertical=True, pills=True, id='nav_buttons', style={"margin-bottom": "50px"}), 
+                              
+], id="sidebar_completa")
 
 
 
